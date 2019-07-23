51b82e86f03dad81e5c5e793592cbfd7254a33ab
yutianqi <66578974@qq.com>
2019-07-23 23:06:47 +0800

    2019.7.23 23:06
    增加约溜详情，细节微调

--- pages/walkingDetail/walkingDetail.js
+++ pages/walkingDetail/walkingDetail.js
@@ -1,40 +1,18 @@
-// pages/my/index.js
-//获取应用实例
-const app = getApp()
-const fn = require('../../utils/fn.js')
-
+// pages/walkingDetail/walkingDetail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    name: '小阳光',
-    sex: 'girl',
-    gouliang: 520,
-    zan: 200,
-    guanzhu: 200,
-    fensi: 190,
-    userInfo:{}
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;
-    wx.getUserInfo({
-      success: function (res) {
 
-        console.log(res);
-        that.data.userInfo = res.userInfo;
-
-        that.setData({
-          userInfo: that.data.userInfo
-        })
-      }
-    })
-    
   },
 
   /**
@@ -84,21 +62,5 @@ Page({
    */
   onShareAppMessage: function () {
 
-  },
-  link: function (e) {
-    fn.link(e.currentTarget.dataset.url)
-  },
-  goAddPet: function () {
-    wx.navigateTo({
-      url: '/pages/addPet/addPet',
-    })
-  },
-  goFoodRecord:function(){
-    wx.navigateTo({
-      url: '/pages/dogFoodRecord/dogFoodRecord',
-    })
-  },
-  bindGetUserInfo(e) {
-    console.log(e.detail.userInfo)
   }
 })
